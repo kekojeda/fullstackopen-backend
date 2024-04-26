1f3ca06600163f26bc7431825db25a6e5acbed42
Sergio Ojeda <keko@MacBook-Pro-de-Sergio.local>
2024-04-25 21:29:47 -0300

    Add lint standard - excersise 3.22

--- index.js
+++ index.js
@@ -2,35 +2,35 @@ require('dotenv').config()
 const Person = require('./models/person')
 const express = require('express')
 const morgan = require('morgan')
-const cors = require('cors');
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(morgan('tiny'));
+app.use(cors())
+app.use(express.json())
+app.use(morgan('tiny'))
 
-app.use(express.static('dist'));
+app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
-});
+  response.send('<h1>Hello World!</h1>')
+})
 
 app.get('/info', (request, response) => {
   Person.find({}).then((person) => {
-    console.log(person.length);
+    console.log(person.length)
     const totalPersons = `
     <p>Phonebook has info for ${person.length} people </p>
-    <p>${new Date()}</p>`;
-    response.send(totalPersons);
-  });
-});
+    <p>${new Date()}</p>`
+    response.send(totalPersons)
+  })
+})
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then((person) => {
     response.json(person)
-  });
-});
+  })
+})
 
 /*
 GET By ID Mongo
@@ -39,13 +39,13 @@ app.get('/api/person/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then((note) => {
       if (note) {
-        response.json(note);
+        response.json(note)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 /*
 DELETE MOGNO
@@ -53,40 +53,40 @@ DELETE MOGNO
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then((result) => {
-      response.status(204).end();
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 /*
 POST to DB Mongo Atlas
 */
 app.post('/api/persons', (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
   if (!body.name) {
     return response.status(400).json({
-      error: 'name missing',
-    });
+      error: 'name missing'
+    })
   }
   if (!body.number) {
     return response.status(400).json({
-      error: 'number missing',
-    });
+      error: 'number missing'
+    })
   }
 
   const person = new Person({
     name: body.name,
-    number: body.number,
-  });
+    number: body.number
+  })
 
   person
     .save()
     .then((savedPerson) => {
-      response.json(savedPerson);
+      response.json(savedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 /*
 END POST to DB Mongo Atlas
 */
@@ -96,49 +96,49 @@ PUT mongo Update
 */
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
   const person = {
     name: body.name,
-    number: body.number,
-  };
+    number: body.number
+  }
 
   Person.findByIdAndUpdate(request.params.id, person)
     .then((updatedPerson) => {
-      response.json(updatedPerson);
+      response.json(updatedPerson)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 /*
 middleware de manejo
  */
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 // controlador de solicitudes con endpoint desconocido
-app.use(unknownEndpoint);
+app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
+    return response.status(400).json({ error: error.message })
   } else if (error.number === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
+    return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
 // este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
